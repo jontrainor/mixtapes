709893b10111cf98592d0223e92c923fbfa5255c
Jon Trainor <jon.trainor@gmail.com>
2019-06-30 21:34:57 -0500

    add built app

---
+++ build/precache-manifest.f2bdb19a42841258d14386cc0772d481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "645f91d7c93e5153f13e",
+    "url": "/static/css/main.f055d1c6.chunk.css"
+  },
+  {
+    "revision": "645f91d7c93e5153f13e",
+    "url": "/static/js/main.2d467e11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f2fbc04d0fe5330d8339",
+    "url": "/static/css/2.6d673fb2.chunk.css"
+  },
+  {
+    "revision": "f2fbc04d0fe5330d8339",
+    "url": "/static/js/2.19d96e02.chunk.js"
+  },
+  {
+    "revision": "d236b3d250560ecc4e0144c8657a8146",
+    "url": "/index.html"
+  }
+];
