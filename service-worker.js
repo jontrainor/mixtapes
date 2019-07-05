81de82841c0f278ce1c64bb8a1e76f7d9ac76500
Jon Trainor <jon.trainor@gmail.com>
2019-07-05 16:44:54 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "mixtapes.jontrainor.com/precache-manifest.9d06c2743ff3ac5de05904327bc28a6b.js"
+  "/precache-manifest.f2bdb19a42841258d14386cc0772d481.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("mixtapes.jontrainor.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
